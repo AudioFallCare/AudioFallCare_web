6ad7ea51c95058e57e8ae1eead63e7156fe55639
chominseo <singer100@yu.ac.kr>
2026-01-30 10:55:02 +0900

    feat : 피보호자 UI

--- src/pages/Guardian.jsx
+++ src/pages/Guardian.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import alarm from './../assets/alarm.png'
 const Guardian = () => {
+    const [nickname, setNickname] = useState("보호자 아이디") // 임시값으로 보호자 아이디
+
   return (
-    <div>Guardian</div>
+    <div className='font-pretendard flex flex-col justify-center items-center mt-50'>
+        <span className='font-semibold text-xl'>{nickname}님이 회원님의</span>
+        <span className='font-semibold text-xl'>낙상을 감지하고 있습니다.</span>
+
+        <span className='text-[#AFAFAF] text-sm mt-8'>아래 버튼을 클릭하여</span>
+        <span className='text-[#AFAFAF] text-sm'>마이크 설정 권한을 허용해주세요</span>
+        <img src={alarm} className="mt-7"/>
+    </div>
   )
 }
 
