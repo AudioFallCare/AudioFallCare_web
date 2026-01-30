b849a4d7bd9bd6a825b431f698f0ac40c42a034a
chominseo <singer100@yu.ac.kr>
2026-01-30 15:51:16 +0900

    feat : BaseAPI

--- src/apis/api.jsx
+++ src/apis/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://audiofallcare-was-test.onrender.com/api/",
+    baseURL: "https://audiofallcare-was-test.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
