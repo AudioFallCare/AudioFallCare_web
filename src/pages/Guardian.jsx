707577bfcef55fd68748ab380cf47975a929779d
chominseo <singer100@yu.ac.kr>
2026-01-30 10:56:12 +0900

    feat : 마이크 권한 허용

--- src/pages/Guardian.jsx
+++ src/pages/Guardian.jsx
@@ -3,6 +3,15 @@ import alarm from './../assets/alarm.png'
 const Guardian = () => {
     const [nickname, setNickname] = useState("보호자 아이디") // 임시값으로 보호자 아이디
 
+     const requestMicPermission = async () => {
+    try {
+      await navigator.mediaDevices.getUserMedia({ audio: true })
+      alert('마이크 권한이 허용되었습니다.')
+    } catch (error) {
+      alert('마이크 권한이 거부되었습니다. 브라우저 설정을 확인해주세요.')
+    }
+  }
+
   return (
     <div className='font-pretendard flex flex-col justify-center items-center mt-50'>
         <span className='font-semibold text-xl'>{nickname}님이 회원님의</span>
@@ -10,7 +19,11 @@ const Guardian = () => {
 
         <span className='text-[#AFAFAF] text-sm mt-8'>아래 버튼을 클릭하여</span>
         <span className='text-[#AFAFAF] text-sm'>마이크 설정 권한을 허용해주세요</span>
-        <img src={alarm} className="mt-7"/>
+        <img
+        src={alarm}
+        className="mt-7 cursor-pointer"
+        onClick={requestMicPermission}
+        alt="마이크 권한 요청"/>
     </div>
   )
 }
