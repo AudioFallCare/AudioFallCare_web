b0429c5ebe1dd3bd4ffdd0a7e776358639d18ec7
chominseo <singer100@yu.ac.kr>
2026-02-05 13:26:33 +0900

    feat : 우편번호 자동으로 input 에 등록되도록함

--- src/components/AddressSearch.jsx
+++ src/components/AddressSearch.jsx
@@ -1,11 +1,23 @@
+import React from "react";
 import DaumPostcode from "react-daum-postcode";
 
 const AddressSearch = ({ onSelect }) => {
   const handleComplete = (data) => {
-    onSelect(data.address);
+    const zipcode = data.zonecode;       // 우편번호
+    const address = data.roadAddress;    // 도로명 주소
+
+    onSelect({ zipcode, address });
   };
 
-  return <DaumPostcode onComplete={handleComplete} />;
+  return (
+    <div style={{ width: "100%", height: "400px" }}>
+      <DaumPostcode
+        onComplete={handleComplete}
+        autoClose
+        style={{ width: "100%", height: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default AddressSearch;
